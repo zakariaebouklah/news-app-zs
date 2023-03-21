fe0a45ca450b3a452b7b3a6b24a9dc584c3e0aa4
zZ7_aki <zakariae.bouklah18@gmail.com>
2023-03-21 23:23:47 +0000

    Feed Page : Implementing the search functionality and the validation of dates (can get articles in a range of 1 month only in dev mode) ==> next pagination & thinking about what can improve performance

--- src/app/pages/feed/feed.module.ts
+++ src/app/pages/feed/feed.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -17,6 +17,7 @@ import {HomePageModule} from "../home/home.module";
         FeedPageRoutingModule,
         HomePageModule
     ],
-  declarations: [FeedPage]
+  declarations: [FeedPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class FeedPageModule {}
--- src/app/pages/feed/feed.page.ts
+++ src/app/pages/feed/feed.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {LogoutService} from "../../services/logout.service";
 import {StateService} from "../../services/state.service";
+import {FeedService} from "../../services/api/feed.service";
+import {Article} from "../../models/article";
 
 @Component({
   selector: 'app-feed',
@@ -9,10 +11,76 @@ import {StateService} from "../../services/state.service";
 })
 export class FeedPage implements OnInit {
 
-  constructor(public logoutService: LogoutService, private stateService: StateService) { }
+  sortMethod: string = this.feedService.sortBy
+
+  query: string = this.feedService.query
+
+  fromDate: string = this.feedService.fromDate
+  toDate: string = this.feedService.toDate
+
+  minDate: string = this.feedService.minDate
+  maxDate: string = this.feedService.maxDate
+
+  @Input() feedArticles: Article[] = []
+
+  constructor(
+    public logoutService: LogoutService,
+    private stateService: StateService,
+    private feedService: FeedService
+  ) { }
 
   ngOnInit(): void {
+
+    console.log(this.toDate)
+    console.log(this.fromDate)
+
     this.stateService.handleAuthStateChanged()
+
+    // @ts-ignore
+    this.feedService.getFeed().subscribe(({articles}) => {
+      console.log(articles)
+      this.feedArticles = articles
+    })
+  }
+
+  validateDate(): void
+  {
+    if(Date.parse(this.fromDate) > Date.parse(this.toDate))
+    {
+      alert("Date Interval is not logic...");
+      this.fromDate = "";
+      this.toDate = "";
+    }
+    if(Date.parse(this.fromDate) < Date.parse(this.minDate))
+    {
+      alert("lower bound date is incorrect !! make sure that the difference between the two dates is 1 month or less...");
+      this.fromDate = "";
+      this.toDate = "";
+    }
+    if(Date.parse(this.toDate) > Date.parse(this.maxDate))
+    {
+      alert("upper bound date is incorrect !! you cannot exceed today's date...");
+      this.fromDate = "";
+      this.toDate = "";
+    }
+  }
+
+  handleSubmit(): void
+  {
+    // @ts-ignore
+    this.feedService.handleNewUserChoices({q :this.query, from: this.fromDate, to: this.fromDate, sort: this.sortMethod}).subscribe(({articles}) => {
+      console.log(articles)
+      this.feedArticles = articles
+    })
+  }
+
+  handleSortChange(): void
+  {
+    // @ts-ignore
+    this.feedService.handleSortMethodChanges(this.sortMethod).subscribe(({articles}) => {
+      console.log(articles)
+      this.feedArticles = articles;
+    })
   }
 
 }
