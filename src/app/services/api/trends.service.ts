9059f237a410d9f6076d59346ddcc22fa5fa7d54
saidelhabchi <elhabchisaid40@gmail.com>
2023-05-16 22:14:10 +0100

    last modif in trends

--- src/app/services/api/trends.service.ts
+++ src/app/services/api/trends.service.ts
@@ -8,12 +8,21 @@ import { environment } from 'src/environments/environment';
 })
 export class TrendsService {
 
+  private d: Date = new Date();
+  private today: string = this.d.getFullYear()  + "-" + (this.d.getMonth()+1) + "-" + this.d.getDate();
   private trendingUrl:string = `https://newsapi.org/v2/everything?apiKey=${environment.newsKey}`
-  private url = `https://newsapi.org/v2/everything?from=2023-05-05&to=2023-04-29&q=trending&sortBy=publishedAt&apiKey=${environment.newsKey}`
+  private url = `https://newsapi.org/v2/everything?from=${this.getLastWeekDate(new Date())}&to=${this.today}&q=trending&sortBy=publishedAt&apiKey=${environment.newsKey}`
 
   constructor(private http : HttpClient) { }
 
   getTrending() : Observable<object>{
     return this.http.get<object>(this.url);
+  }
+
+  getLastWeekDate(d : Date): string
+  {
+    const previous = new Date(d.getTime());
+    previous.setDate(d.getDate() - 6)
+    return previous.getFullYear() + "-" + (previous.getMonth() + 1) + "-" + previous.getDate();
   }
 }
